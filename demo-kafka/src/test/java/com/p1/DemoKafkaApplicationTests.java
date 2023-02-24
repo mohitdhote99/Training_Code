2d4ada930a961e6ffc37e5191b6cdab1e38c960d
PBKVijayKulkarni <48426605+PBKVijayKulkarni@users.noreply.github.com>
2023-02-24 16:18:10 +0530

    Add files via upload

---
+++ demo-kafka/src/test/java/com/p1/DemoKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.p1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
